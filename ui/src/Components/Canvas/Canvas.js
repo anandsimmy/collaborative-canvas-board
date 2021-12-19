ddf14a3ec44f4811333684341e237ac6e362eeda
Anand Simmy <anandsimmy7@gmail.com>
2021-12-19 08:47:27 +0530

    added canvas

---
+++ ui/src/Components/Canvas/Canvas.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import CanvasBoard from '../CanvasBoard/CanvasBoard';
+
+const Canvas= () => {
+    return (
+    <div>
+        <input type='color' />
+        <CanvasBoard />
+    </div>
+    )
+
+}
+
+export default Canvas
