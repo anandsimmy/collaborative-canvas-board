34b8f370528f17c73265ad057ea8d1e6515926d8
Anand Simmy <anandsimmy7@gmail.com>
2021-12-20 08:47:48 +0530

    minor changes

---
+++ ui/src/Components/PaintingBoard/PaintingBoard.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Canvas from '../Canvas/Canvas';
+import './PaintingBoard.css';
+
+const PaintingBoard= () => {
+    return (
+    <div className='wrapper'>
+        <Canvas />
+    </div>
+    )
+
+}
+
+export default PaintingBoard
