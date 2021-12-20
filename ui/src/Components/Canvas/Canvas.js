8971edb418df82e3b82072da9f75bc8bd2312b2f
Anand Simmy <anandsimmy7@gmail.com>
2021-12-20 15:28:24 +0530

    server changes

--- ui/src/Components/Canvas/Canvas.js
+++ ui/src/Components/Canvas/Canvas.js
@@ -11,7 +11,7 @@ const Canvas = () => {
     }
 
     useEffect(() => {
-        canvasDetails.socket = io.connect('http://192.168.1.4:5000', () => {
+        canvasDetails.socket = io.connect('http://localhost:5000', () => {
             console.log('connecting to server')
         })
         canvasDetails.socket.on('image-data', (data) => {
